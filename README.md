04467c583ca93ac6f95178576b722944acd1c109
Kevin Depue <1028792+wusticality@users.noreply.github.com>
2024-01-07 14:22:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rust-template
+
+My rust template on NixOS.
---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "My rust template.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { system = system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustup
+            # pkg-config              
+            # gcc
+            # alsa-lib
+            # udev
+            # vulkan-loader
+            # xorg.libX11
+            # xorg.libXrandr
+            # xorg.libXcursor
+            # xorg.libXi
+            # tracy
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              # pkgs.alsa-lib
+              # pkgs.udev
+              # pkgs.vulkan-loader
+              # pkgs.xorg.libX11
+              # pkgs.xorg.libXrandr
+              # pkgs.xorg.libXcursor
+              # pkgs.xorg.libXi
+          ];
+        };
+      });
+    };
+}
